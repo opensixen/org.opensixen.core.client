cd66ad647877ba06d6204e3738976e368f165aff
Eloy Gomez <eloy@indeos.es>
2010-09-02 09:58:41 +0000

    Añadimos HelperContentProvider AccountDataHelperContentProvider y añadimos las extensiones necesarias a MANIFEST.MF

---
+++ src/org/opensixen/swing/AccountDataHelperContentProvider.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package org.opensixen.swing;
+
+import javax.swing.table.TableModel;
+
+import org.compiere.model.MQuery;
+import org.compiere.util.DisplayType;
+import org.opensixen.interfaces.IBeanProvider;
+import org.opensixen.model.AccountDataBeanProvider;
+import org.opensixen.model.ColumnDefinition;
+
+/**
+ * 
+ * 
+ * @author Eloy Gomez
+ * Indeos Consultoria http://www.indeos.es
+ *
+ */
+public class AccountDataHelperContentProvider extends AbstractTableHelperContentProvider {
+
+	/* (non-Javadoc)
+	 * @see org.opensixen.swing.AbstractTableHelperContentProvider#getTableModel(org.compiere.model.MQuery)
+	 */
+	@Override
+	public TableModel getTableModel(MQuery query) {
+		IBeanProvider beanProvider = new AccountDataBeanProvider(ctx);
+		BeanTableModel tableModel = new BeanTableModel(beanProvider, getColumnDefinitions());	
+		return tableModel;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.opensixen.swing.AbstractTableHelperContentProvider#initTable()
+	 */
+	@Override
+	public void initTable() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public ColumnDefinition[] getColumnDefinitions() {
+		ColumnDefinition[] cols = {
+				new ColumnDefinition("value", "Value", 80, DisplayType.String),
+				new ColumnDefinition("name", "Nombre", 180, DisplayType.String),				
+				new ColumnDefinition("haber", "Haber", 150, DisplayType.Amount) ,
+				new ColumnDefinition("debe", "Debe", 150, DisplayType.Amount),
+				new ColumnDefinition("saldoInicial", "Inicial", 150, DisplayType.Amount) };
+		return cols;
+	}
+
+}
